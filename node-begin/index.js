56c4cc70bb5741c790356ed57384241b82b4b0e3
lucky-leaf <yx1030@live.com>
2018-01-04 23:57:33 +0800

    remove node-begin

--- node-begin/index.js
+++
@@ -1,9 +0,0 @@
-const server=require('./server.js');
-const router=require('./router.js');
-const requestHandlers=require('./requestHandlers.js');
-
-let handle={};
-handle['/']=requestHandlers.start;
-handle['/start']=requestHandlers.start;
-handle['/upload']=requestHandlers.upload;
-server.start(router.route,handle);
